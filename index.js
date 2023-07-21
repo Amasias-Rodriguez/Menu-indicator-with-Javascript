3f8b4a6a1e9be9179ceb307bad768b4642807f34
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-21 00:24:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+let marker = document.querySelector('#indicator');
+let nav = document.querySelector('nav');
+let item = document.querySelectorAll('nav a');
+
+nav.onclick = function () {
+    marker.classList.toggle('change');
+}
+
+function indicator(e) {
+    marker.computedStyleMap.left = e.offsetLeft + 'px';
+    marker.style.width = e.offsetWidth + 'px';
+    marker.style.display = "block";
+    marker.display.filter = 'hue-rotate(' + Math.random() * 360 + 'deg)'
+}
+
+item.forEach(link => {
+    link.addEventListener("click", (e) => {
+        indicator(e.target);
+    })
+})
+
+function addActiveClass() {
+    item.forEach((i) =>
+        i.classList.remove('active'));
+    this.classList.add('active');
+}
